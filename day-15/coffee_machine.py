2c5edfe0d10a2c23c36694374e0bbecf98182284
Reuben deVries <reubendevries@gmail.com>
2023-01-09 21:45:31 -0800

    Finished Day-15

--- day-15/coffee_machine.py
+++ day-15/coffee_machine.py
@@ -12,35 +12,36 @@ def coffee_menu(report):
     
     order = input('What would you like? (espresso/latte/cappuccino): ').lower()
     if order == 'espresso':
-        payment(menu[0]['Price'],report)
+        payment(menu[0]['Price'], menu[0]['Name'], report)
         report = make_espresso(menu[0], report) 
     elif order == 'latte':
-        payment(menu[1]['Price'], report)
+        payment(menu[1]['Price'], menu[0]['Name'], report)
         report =  make_latte(menu[1], report)
     elif order == 'make_cappucino':
-        payment(menu[2]['Price'], report)
+        payment(menu[2]['Price'], menu[0]['Name'], report)
         report = make_cappucino(menu[2], report)
     else:
         print_report(report)
 
-def payment(price, report):
+def payment(price, drink, report):
     payment_completed = False
+    print( f'The price of the {drink} is ${price:.2f}' )
     while not payment_completed:
         print('Please insert coins')
         quarters = float( input( 'How many quarters?: ') )
         dimes = float( input( 'How many dimes?: ') )
-        nickels = float( input( 'How many nickels?: ') )
+        nickels = float( input( 'How many nickels?: ') )    
         pennies = float( input( 'How many pennies?: ') )
-        total = (quarters * 0.25) + (dimes * 0.10) + (nickels * 0.05) + (pennies * 0.01)
+        total = round( (quarters * 0.25) + (dimes * 0.10) + (nickels * 0.05) + (pennies * 0.01), 2 )
         if total < price:
             amount_left = (price - total)
-            print( f'You\'ve not put enough money in. Please put {amount_left} to cover the rest of the cost of the coffee' )
+            print( f'You\'ve not put enough money in. Please put {amount_left:.2f} to cover the rest of the cost of the coffee' )
         else:
             payment_completed = True
     report['Money'] += price
     change = ( total - price )
     if change > 0.0:
-        print( f'Here is { change } in change.' )
+        print( f'Here is { change:.2f } in change.' )
     else:
         print('Payment recieved, a fresh cup of coffee is being made for you.')
 
@@ -82,7 +83,7 @@ def print_report(report):
 if __name__ == '__main__':
     while enough_ingredients:
         coffee_menu(report)
-        if report['Coffee'] > 18 or report['Water'] > 50 or report['Milk'] > 0:
+        if report['Coffee'] < 24 or report['Water'] < 250 or report['Milk'] < 150:
                 print( 'WARNING: You don\'t have enough supplies to make another drink.')
                 if input( 'Do you want to resupply your Coffee Machine?: (choose \'yes\' or \'no\') ' ) == 'yes':
                     resupply_coffee_machine(report)
